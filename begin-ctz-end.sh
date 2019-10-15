6466c430a1752c374bfee17076b60a834574778d
Seongjun Ji <metalg0su@gmail.com>
2019-10-15 12:07:52 +0900

    fdsa

---
+++ begin-ctz-end.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. scripts/stop_all.sh
+rm -rf .sto* *.tgz log
+
+git checkout 2.2.1.3 -f
+source venv/bin/activate
+pip install -e .
+make generate-proto
+
+clear
+python -V
+git show --oneline
+
+loop citizen -r 172.31.18.224:9000 -d -o peer_config.json
