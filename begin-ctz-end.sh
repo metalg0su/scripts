15b8d965d08ead86c803104adfefc46ecc6e2974
Seongjun Ji <metalg0su@gmail.com>
2019-10-15 17:56:30 +0900

    fdsfdsa

--- begin-ctz-end.sh
+++ begin-ctz-end.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-. _begin_common.sh
+. scripts/_begin_common.sh
 loop citizen -r 172.31.18.224:9000 -d -o peer_config.json
--- begin-ctz-group.sh
+++ begin-ctz-group.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-. _begin_common.sh
+. scripts/_begin_common.sh
 loop citizen -r 172.31.27.207:9000 -d -o peer_config.json
