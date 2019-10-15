254fd93261c5d5fdc76c74c5db171be615a17982
Seongjun Ji <metalg0su@gmail.com>
2019-10-15 17:10:54 +0900

    fdsafdsa

--- _begin_common.sh
+++ _begin_common.sh
@@ -2,6 +2,7 @@
 . scripts/stop_all.sh
 rm -rf .sto* *.tgz log
 
+. scripts/set-rc-100.sh
 git checkout origin/2.2.1.3-test -f
 
 . scripts/_setup_common.sh
--- new-version.sh
+++ new-version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . scripts/stop_all.sh
+. scripts/set-rc-111.sh
 git checkout origin/feature/LC-685-test -f
 
 . scripts/_setup_common.sh
