b5e83f83a14885d06e44f98c3e9653f02be1e124
Seongjun Ji <metalg0su@gmail.com>
2019-10-15 18:07:30 +0900

    fdsafdsa

--- new-version.sh
+++ new-version.sh
@@ -3,6 +3,12 @@
 . scripts/set-rc-111.sh
 git checkout origin/feature/LC-685-test -f
 
-. scripts/_setup_common.sh
+source venv-new/bin/activate
+pip install -e .
+make generate-proto
+
+clear
+python -V
+git show --oneline
 
 loop -d -o peer_config.json
