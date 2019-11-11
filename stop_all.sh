db0209b8aaef587d134f72f866973fc0d3a0e395
Seongjun Ji <metalg0su@gmail.com>
2019-11-11 16:15:39 +0900

    docker container reset

--- stop_all.sh
+++ stop_all.sh
@@ -8,6 +8,10 @@ for i in $(seq 3 8); do
  eval ${check_ps}
 done
 
+# Rabbitmq reset
 sudo rabbitmqctl stop_app
 sudo rabbitmqctl reset
 sudo rabbitmqctl start_app
+
+mq_container_id=`sudo docker ps | grep rabbitmq | cut -d " " -f1`
+sudo docker restart ${mq_container_id}
